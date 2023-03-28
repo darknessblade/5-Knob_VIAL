6216c2735c9410d571bebe37a0e4a99687b462e7
darknessblade <darknessbladers@gmail.com>
2023-03-28 15:22:24 +0200

    Add files via upload

---
+++ 5knob/config.h
@@ -0,0 +1,12 @@
+// Copyright 2022 Victor Lucachi (@Victor Lucachi)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 11
+//#define DYNAMIC_KEYMAP_MACRO_EEPROM_ADDR + 896
+//#define DYNAMIC_KEYMAP_MACRO_EEPROM_SIZE 896
+#define DYNAMIC_KEYMAP_MACRO_COUNT 32
+//#define DYNAMIC_MACRO_SIZE 256
